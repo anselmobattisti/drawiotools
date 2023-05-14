a9e49a25f4970a06ca5fe2e491744b9407251930
Anselmo Battisti <anselmo@webgenium.com.br>
2023-05-13 21:07:13 -0300

    Using page-range instead of page-index

--- exportpdf.sh
+++ exportpdf.sh
@@ -13,10 +13,15 @@ drawio --export --format xml --uncompressed "$file.drawio"
 # Use xmllint to extract the name attribute of all diagram elements
 pages=($(xmllint --xpath "//diagram/@name" "$file.xml" 2>/dev/null | sed -e 's/ name=//g' -e 's/\"//g'))
 
+
 i=0
 for page in "${pages[@]}"; do    
-    drawio --export --page-index $i --output "$file-$page.pdf" "$file.drawio"
-    ((i=i+1))    
+
+    # The --page-index option is not working (https://github.com/jgraph/drawio/issues/3597), so we are using --page-range instead.
+    # drawio --export --embed-diagram --crop --page-index $i --output "$file-$i.pdf" "$file.drawio"
+
+    drawio --export --embed-diagram --crop --page-range $i..$i --crop --output "$file-$page.pdf" "$file.drawio"    
+    ((i=i+1))
 done
 
 # remove the tmp xml file
